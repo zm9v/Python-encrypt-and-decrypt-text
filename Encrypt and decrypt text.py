8c83a3477ad35edf0056241f30820ab407e5b4a5
Abdulrahman Al Satary <91272715+zm9v@users.noreply.github.com>
2022-01-04 08:35:44 +0300

    Update Encrypt and decrypt text.py

--- Encrypt and decrypt text.py
+++ Encrypt and decrypt text.py
@@ -1,7 +1,5 @@
 import random
 
-import os
-
 try:
     
     from colorama import *
@@ -14,7 +12,24 @@ except:
         
     except:
 
-        os.system('pip3 install colorama')        
+        os.system('pip3 install colorama')
+        
+def ADD_TEXT(LIST, INPUT, NUMBER):
+    
+    for i in range(len(INPUT)):
+        
+        LIST.append(INPUT[NUMBER])
+        
+        NUMBER+= 1
+        
+def ENCRYPT(TEXT, INDEX, LIST):
+    
+    for m in range(len(TEXT)):
+        
+        TEXT[INDEX:INDEX] = random.choices(LIST, k = 20)
+        
+        INDEX += 21
+
 
 print(f'''{Fore.RED}
   ________  __  _____      __
@@ -30,33 +45,42 @@ print(f'''{Fore.RED}
 2 To decrypt
 ''')
 
-Choice,Text,Index0,List,Index = input(f'{Fore.RED}> ').strip(),[],0,'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789,./\';][=-<>?:@{}_)(*&^%$£"!',1
+List = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789,./\';][=-<>?:@{}_)(*&^%$£"!'
+Choice = input(f'{Fore.RED}> ').strip()
+List0 = []
+List2 = []
+Index0 = 0
+Index02 = 0
+Index = 1
+Index2 = 1
 
 if Choice == '1':
     
     Input = input(f'{Fore.RED}Text : ').strip()
     
-    for i in range(len(Input)):
-        
-        Text.append(Input[Index0])
-        
-        Index0 += 1
-        
-    for m in range(len(Text)):
-        
-        Text[Index:Index] = random.choices(List, k = 2)
-        
-        Index += 3
-        
-    print(''.join(Text))
+    ADD_TEXT(List0, Input, Index0)
     
+    ENCRYPT(List0, Index, List)
+    
+    ADD_TEXT(List2, List0, Index02)
+    
+    ENCRYPT(List2, Index2, List)
+    
+    print(''.join(List2))
+
+    print(f'{Fore.RED}Done :)')
+
     input(f'{Fore.RED}Press enter to close ...')
     
 elif Choice == '2':
     
     Input2 = Input = input(f'{Fore.RED}Text : ').strip()
     
-    print(Input2[::3])
+    Decrypt = Input2[::21]
+
+    print(Decrypt[::21])
+
+    print(f'{Fore.RED}Done :)')
     
     input(f'{Fore.RED}Press enter to close ...')
     
@@ -65,3 +89,4 @@ else:
     print(f'{Fore.RED}Wrong input :(')
     
     input(f'{Fore.RED}Press enter to close ...')
+
